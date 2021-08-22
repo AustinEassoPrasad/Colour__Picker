0a49cbd4bcaa9702a832d948439baab8c90c3417
AustinEassoPrasad <62349432+AustinEassoPrasad@users.noreply.github.com>
2021-08-22 09:06:24 +0530

    Add files via upload

---
+++ austin.js
@@ -0,0 +1,28 @@
+function red() {
+    document.body.style.backgroundColor = "red";
+}
+
+function orange() {
+    document.body.style.backgroundColor = "orange";
+}
+
+function yellow() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+function green() {
+    document.body.style.backgroundColor = "green";
+}
+
+function blue() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function indigo() {
+    document.body.style.backgroundColor = "indigo";
+}
+
+
+function violet() {
+    document.body.style.backgroundColor = "violet";
+}
